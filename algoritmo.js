8573cd6d89f0938953608238ec186b1166df6e86
Leandro, Roberto y Rafa <prueba@gmail.com>
2023-03-21 11:59:46 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+document.querySelector("body").innerHTML = `
+<iframe width="560" height="315" src="https://www.youtube.com/embed/d8bGiKDsp74" title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+
+    <p>descripcion</p>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13136.066850016401!2d-58.3815704!3d-34.6037389!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1679403371321!5m2!1ses!2sar"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <iframe style="border-radius:12px"
+        src="https://open.spotify.com/embed/track/6gwaa6ElIixNTvu6RwkMyo?utm_source=generator" width="100%" height="352"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <p>musica rock</p>
+
+    <iframe height="500px" width="1000px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+`;
